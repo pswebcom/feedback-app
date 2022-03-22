031969a14dda4cf3a1061ba325abc4e34742e896
pswebcom <pswebco@gmail.com>
2022-03-22 17:56:04 -0400

    Card component--with conditions--styles added

--- app/src/components/FeedbackItem.js
+++ app/src/components/FeedbackItem.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Card from "./shared/Card";
 
 const FeedbackItem = ({ item }) => {
   return (
-    <div className="card">
-      <div className="num-display">{item.rating}</div>
+    <Card reverse={item.reverse} id={item.id}>
+      <div className="num-display">
+        <span>{item.rating}</span>
+      </div>
       <div className="text-display">
         <span>{item.text}</span>
       </div>
-    </div>
+    </Card>
   );
 };
 
--- app/src/data/FeedbackData.js
+++ app/src/data/FeedbackData.js
@@ -2,21 +2,25 @@ const FeedbackData = [
   {
     id: 1,
     rating: 10,
+    reverse: false,
     text: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. ",
   },
   {
     id: 2,
     rating: 6,
+    reverse: true,
     text: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. ",
   },
   {
     id: 3,
     rating: 7,
+    reverse: false,
     text: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. ",
   },
   {
     id: 4,
     rating: 4,
+    reverse: true,
     text: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. ",
   },
 ];
