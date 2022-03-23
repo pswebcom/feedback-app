94fc0644587f007344dc1ef64a818cb597b11ca6
pswebcom <pswebco@gmail.com>
2022-03-23 13:35:21 -0400

    close functionality in card component--delete event drill--feedbackItem--feedbackList--App.js

--- app/src/App.js
+++ app/src/App.js
@@ -5,12 +5,19 @@ import { useState } from "react";
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure ?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
     <>
       <Header />
       <div className="container">
         <div className="feedback">
-          <FeedbackList feedback={feedback} />
+          <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
         </div>
       </div>
     </>
--- app/src/components/FeedbackItem.js
+++ app/src/components/FeedbackItem.js
@@ -1,17 +1,30 @@
 import React from "react";
 import Card from "./shared/Card";
+import PropTypes from "prop-types";
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, handleDelete }) => {
   return (
     <Card reverse={item.reverse} id={item.id}>
       <div className="num-display">
         <span>{item.rating}</span>
       </div>
+      <button className="close" onClick={() => handleDelete(item.id)}>
+        <i
+          className="fa fa-close"
+          style={{
+            color: item.id % 2 === 0 ? "#fff" : "#00162b",
+          }}
+        ></i>
+      </button>
       <div className="text-display">
         <span>{item.text}</span>
       </div>
     </Card>
   );
+};
+
+FeedbackItem.protoTypes = {
+  item: PropTypes.object.isRequired,
 };
 
 export default FeedbackItem;
